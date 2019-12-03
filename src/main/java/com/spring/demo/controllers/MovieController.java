94a0248b324e7d11c4360ecc3b9d202208c3d469
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2019-12-03 18:11:44 +0100

    MovieService init

--- src/main/java/com/spring/demo/controllers/MovieController.java
+++ src/main/java/com/spring/demo/controllers/MovieController.java
@@ -1,5 +1,33 @@
 package com.spring.demo.controllers;
 
+
+import com.spring.demo.entities.Movie;
+import com.spring.demo.services.MovieCache;
+import com.spring.demo.services.MovieService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("api/movies")
 public class MovieController {
 
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Movie> getMovie(@RequestParam(defaultValue = "") String i) {
+        System.out.println(MovieCache.getMovieCache().size());
+        var movie = movieService.getMovieById(i);
+        System.out.println(MovieCache.getMovieCache().size());
+        return new ResponseEntity<>(movie, HttpStatus.OK);
+    }
 }
--- src/main/java/com/spring/demo/services/MovieService.java
+++ src/main/java/com/spring/demo/services/MovieService.java
@@ -1,4 +1,32 @@
 package com.spring.demo.services;
 
+
+import com.spring.demo.db.MovieRepository;
+import com.spring.demo.entities.Movie;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public Movie getMovieById(String id) {
+
+        var cachedMovie = MovieCache.getMovieFromCache(id);
+        if (cachedMovie.isPresent()) {
+            System.out.println("Cached movie");
+            return cachedMovie.get();
+        } else {
+            // Add request to OmdbService
+            Movie movie = new Movie();
+            movie.setId(id);
+
+            MovieCache.addMovieToCache(movie);
+            return movie;
+        }
+    }
 }
