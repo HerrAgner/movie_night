47be32fa583d4c57d364b04c8c6825e6472f5146
Rami <rami.albadri77@gmail.com>
2019-12-03 16:36:08 +0100

    navbar added

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card flat id="nav">
+    <v-bottom-nav :value="true" absolute color="white">
+      <v-btn to="/" color="teal" flat value="home">
+        <span>Home</span>
+      </v-btn>
+
+      <v-btn
+        to="/login"
+        color="teal"
+        flat
+        value="LoginRegister"
+        v-if="!statusT"
+      >
+        <span>Login</span>
+      </v-btn>
+
+    </v-bottom-nav>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+#nav {
+  height: 56px;
+  z-index: 999;
+}
+
+#alert {
+  color: red;
+}
+</style>
