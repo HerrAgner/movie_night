f157435b00b7be8f90141330f4055785cb738f88
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2019-12-03 18:12:27 +0100

    MovieCache with some methods

--- src/main/java/com/spring/demo/services/MovieCache.java
+++ src/main/java/com/spring/demo/services/MovieCache.java
@@ -1,4 +1,42 @@
 package com.spring.demo.services;
 
+import com.spring.demo.db.MovieRepository;
+import com.spring.demo.entities.Movie;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
 public class MovieCache {
+
+    private static Set<String> movieCache;
+    private static MovieRepository movieRepository;
+
+    public MovieCache(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+        movieCache = movieRepository.findAll().stream().map(m -> m.getId()).collect(Collectors.toSet());
+    }
+
+    public static Set<String> getMovieCache() {
+        return movieCache;
+    }
+
+    public void setMovieCache(Set<String> movieCache) {
+        this.movieCache = movieCache;
+    }
+
+    public static void addMovieToCache(Movie movie) {
+        movieCache.add(movie.getId());
+        System.out.println(movie.getId());
+        movieRepository.insert(movie);
+    }
+
+    public static Optional<Movie> getMovieFromCache(String movieId) {
+        if (movieCache.contains(movieId)) {
+            System.out.println("Returning " + movieId + " from cache");
+            return movieRepository.findById(movieId);
+        } else return Optional.empty();
+    }
 }
