0484893f49e2a365e9e27f92389328cd8ba67ccb
Ted <ted.agner@hotmail.com>
2019-12-03 15:48:24 +0100

    Changed movie-class

--- src/main/java/com/spring/demo/entities/Movie.java
+++ src/main/java/com/spring/demo/entities/Movie.java
@@ -5,36 +5,177 @@ import org.springframework.data.annotation.Id;
 public class Movie {
 
     @Id
-    private String id ;
+    private String id;
     private String title;
+    private int year;
+    private String rated;
+    private String released;
+    private String[] Genre;
+    private String director;
+    private String[] writer;
+    private String[] actors;
+    private String plot;
+    private String[] language;
+    private String country;
+    private String awards;
+    private String poster;
+    private String metascore;
     private float imdbRating;
+    private int imdbVotes;
+    private String type;
+    private String production;
+
+    public Movie(){}
 
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public float getImdbRating() {
-        return imdbRating;
-    }
-
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public String[] getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String[] genre) {
+        Genre = genre;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String[] getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String[] writer) {
+        this.writer = writer;
+    }
+
+    public String[] getActors() {
+        return actors;
+    }
+
+    public void setActors(String[] actors) {
+        this.actors = actors;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public String[] getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String[] language) {
+        this.language = language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getMetascore() {
+        return metascore;
+    }
+
+    public void setMetascore(String metascore) {
+        this.metascore = metascore;
+    }
+
+    public float getImdbRating() {
+        return imdbRating;
     }
 
     public void setImdbRating(float imdbRating) {
         this.imdbRating = imdbRating;
     }
 
-    public Movie() {
+    public int getImdbVotes() {
+        return imdbVotes;
     }
 
-    public Movie(String title, float imdbRating) {
-        this.title = title;
-        this.imdbRating = imdbRating;
+    public void setImdbVotes(int imdbVotes) {
+        this.imdbVotes = imdbVotes;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getProduction() {
+        return production;
+    }
+
+    public void setProduction(String production) {
+        this.production = production;
+    }
 }
