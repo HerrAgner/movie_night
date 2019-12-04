620a573305687ed79da88b54cc1f4cd3b08b2a9b
Ted <ted.agner@hotmail.com>
2019-12-04 15:01:35 +0100

    Added function to searchbar

--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -3,17 +3,32 @@
             class="searchField"
             outlined
             label="Search"
-            append-icon="search">
+            append-icon="search"
+            v-model="search"
+            v-on:keyup.enter="searchForMovies">
     </v-text-field>
+    
 </template>
 
 <script>
   export default {
-    name: "SearchInput"
+    name: "SearchInput",
+    data () {
+      return {
+        search: ""
+      }
+    },
+    methods: {
+      async searchForMovies() {
+          let response = await fetch('api/movies/search?s=' + this.search);
+          response = response.status === 200 ? response.json() : null;
+          return response;
+      }
+    }
   }
+
 </script>
 
 <style scoped>
-    .searchField {
-    }
+
 </style>
