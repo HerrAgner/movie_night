e0626f36051623bebd82f7d5b2e0b8cc6ec17dbb
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2019-12-04 11:49:33 +0100

    Redirect path from backend

--- src/main/java/com/spring/demo/controllers/DefaultController.java
+++ src/main/java/com/spring/demo/controllers/DefaultController.java
@@ -10,28 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class DefaultController {
 
-//    @GetMapping("/")
-//    public ModelAndView index() {
-//        return new ModelAndView("index.html");
-//    }
-
-//    @GetMapping("/**/{path:[^?\\.]+}/")
-//    public ModelAndView path(@PathVariable String path, HttpServletRequest request){
-////        Push the path
-//        String pushPath = "redirect:/?redirect=" + request.getRequestURI();
-////        Check if redirect route
-//        pushPath = request.getQueryString() != null ? pushPath.concat("?" + request.getQueryString()) : pushPath;
-//        return new ModelAndView(pushPath);
-//    }
-
     @GetMapping("/**/{path:[^?\\.]+}/")
     public ModelAndView path(@PathVariable String path, HttpServletRequest request){
         String path2push = "redirect:/?redirect="+request.getRequestURI();
-        System.out.println(request.getRequestURI().substring(1));
         path2push = request.getQueryString() != null ? path2push.concat("?" + request.getQueryString()) : path2push;
         System.out.println(request.getQueryString());
         return new ModelAndView(path2push);
     }
-
-
 }
