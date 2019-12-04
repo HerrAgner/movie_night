5d6b219b7561554c51927f6d14948ebf27ec3482
Ted <ted.agner@hotmail.com>
2019-12-04 16:29:17 +0100

    Search works

--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -1,28 +1,50 @@
 <template>
-    <v-text-field
-            class="searchField"
-            outlined
-            label="Search"
-            append-icon="search"
-            v-model="search"
-            v-on:keyup.enter="searchForMovies">
-    </v-text-field>
+    <v-container>
+        <v-text-field
+                class="searchField"
+                outlined
+                label="Search"
+                append-icon="search"
+                v-model="search"
+                v-on:keyup.enter="searchForMovies"
+                style="position: absolute; width: 30%; top: 5px">
+        </v-text-field>
+        <v-list style="position: absolute; top: 6vh; width: 30%">
+            <SearchResult v-for="search in searches"
+                          :key="search.imdbID"
+                          :poster="search.Poster"
+                          :title="search.Title"
+                          :year="search.Year"
+                          :id="search.imdbID"/>
+        
+        </v-list>
     
+    </v-container>
 </template>
 
 <script>
+  import SearchResult from "./SearchResult";
+
   export default {
     name: "SearchInput",
-    data () {
+    components: {SearchResult},
+    data() {
       return {
-        search: ""
+        search: "",
+        searchResponse: []
       }
     },
     methods: {
       async searchForMovies() {
-          let response = await fetch('api/movies/search?s=' + this.search);
-          response = response.status === 200 ? response.json() : null;
-          return response;
+        let response = await fetch('api/movies/search?s=' + this.search);
+        response = response.status === 200 ? await response.json() : null;
+        this.searchResponse = response.Search;
+        return response;
+      }
+    },
+    computed: {
+      searches() {
+        return this.searchResponse;
       }
     }
   }
--- frontend/src/components/SearchResult.vue
+++ frontend/src/components/SearchResult.vue
@@ -1,10 +1,34 @@
 <template>
-    
+    <div style="display: flex; flex-direction: column">
+        <v-list-item
+                :key="title"
+                @click="goToMovie"
+        >
+            <v-list-item-avatar style="height: min-content" tile>
+                <v-img :src="poster"/>
+            </v-list-item-avatar>
+            
+            <v-list-item-content>
+                <v-list-item-title v-html="title"/>
+                <v-list-item-subtitle v-html="year"/>
+            </v-list-item-content>
+        </v-list-item>
+        <v-divider/>
+    </div>
 </template>
 
 <script>
   export default {
-    name: "SearchResult"
+    name: "SearchResult",
+    data() {
+      return {}
+    },
+    props: ['title', 'year', 'poster', 'id'],
+    methods: {
+      goToMovie() {
+        this.$router.push({path: "details?id=" + this.id})
+      }
+    }
   }
 </script>
 
