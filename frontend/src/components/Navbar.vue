1bbfab5be3523385df1bd282e13ea6ec12519693
Rami <rami.albadri77@gmail.com>
2019-12-04 11:25:49 +0100

    logout button shuold show up if you are loggedin

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -8,9 +8,20 @@
         to="/login"
         color="teal"
         text
-        value="LoginRegister"
+        value="login"
+        v-if="!isLoggedin"
       >
         <span>Login</span>
+      </v-btn>
+
+        <v-btn
+        color="teal"
+        text
+        value="log out"
+        v-if="isLoggedin"
+        @click="changeStatus"
+      >
+        <span>Log out </span>
       </v-btn>
 
   </v-card>
@@ -19,6 +30,31 @@
 <script>
 export default {
   name: "Navbar",
+      data(){
+        return {
+            isLoggedin: this.$store.state.isLoggedin
+        }
+    },
+    methods:{
+      changeStatus(){
+          this.$store.dispatch('logout', this.$store.state.userInfo.email)
+          
+      }
+    },
+    watch: {
+      status(newValue) {
+        if (newValue === true) {
+          this.isLoggedin = true;
+        } else {
+          this.isLoggedin = false;
+        }
+      }
+  },
+    computed: {
+      status() {
+        return this.$store.state.isLoggedin
+      }
+  }
 };
 </script>
 
