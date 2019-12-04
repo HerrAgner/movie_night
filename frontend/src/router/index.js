1cf6bd97d67e7228271e1e393003e24f104f081b
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2019-12-04 12:19:09 +0100

    Redirects to home when a path does not exist

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -13,16 +13,22 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')  },
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/*'
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
