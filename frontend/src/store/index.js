b7d929e7b95c71734b092d50b41883ddc0f9c1f7
Rami <rami.albadri77@gmail.com>
2019-12-04 11:24:38 +0100

    set cookie if is loggedin

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,37 +1,40 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Cookie from "js-cookie";
+import router from '../router/index.js'
+
+
 
 Vue.use(Vuex);
-//const LOGIN_URL = "api/login";
 
 export default new Vuex.Store({
   state: {
-    islogedin: false
+    isLoggedin: false
   },
   mutations: {
-    setStatus(state, status) {
-      state.islogedin = status;
+    setLogin(state, status) {
+      state.isLoggedin = status;
     }
   },
   actions: {
     async login(context, info) {
       const data = { username: info.email, password: info.password };
-      let response = await fetch('/api/login', {
+      await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-      });
-      response = response.status === 200 ? await response.json() : null;
-      console.log(response);
-      //let successfulLogin = !response.url.includes("error");
-      //if (successfulLogin) {
-      //this.commit("setStatus", successfulLogin);
-      //router.push({ path: '/' })
-      //this.dispatch('getUserInfoFromDb', info.email)
-      //}
-    }
+      })
+      .then(async response => {
+        this.response = response.status === 200 ? await response.json() : null;
+        if (this.response !== null) {
+          Cookie.set("token", this.response.jwt);
+          this.commit("setLogin", true);
+          router.push({ path: '/' })
+        }
+      })
+    },
   },
   modules: {}
 });
