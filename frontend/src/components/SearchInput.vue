d6aed7777e93dde4a499e387b9c376317487c190
Ted <ted.agner@hotmail.com>
2019-12-05 11:32:17 +0100

    Menu closes on blur

--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -8,9 +8,10 @@
                 v-model="search"
                 v-on:keyup.enter="searchForMovies"
                 style="position: absolute; width: 30%; top: 5px"
+                v-on:blur="timedCloseMenu"
         >
         </v-text-field>
-        <v-list style="position: absolute; top: 6vh; width: 30%">
+        <v-list v-if="searchResponse.length > 0" style="position: absolute; top: 6vh; width: 30%">
             <SearchResult v-for="search in searches"
                           :key="search.imdbID"
                           :poster="search.Poster"
@@ -46,6 +47,9 @@
       closeMenu() {
         this.search = "";
         this.searchResponse = [];
+      },
+      timedCloseMenu() {
+        setTimeout(this.closeMenu, 100)
       }
     },
     computed: {
