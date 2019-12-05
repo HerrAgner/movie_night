30cc274f70a20c7cb79d334e14f4c0fdc53278bf
Ted <ted.agner@hotmail.com>
2019-12-05 12:13:23 +0100

    Added loading when searching for movies

--- frontend/src/components/SearchInput.vue
+++ frontend/src/components/SearchInput.vue
@@ -6,43 +6,54 @@
                 label="Search"
                 append-icon="search"
                 v-model="search"
-                v-on:keyup.enter="searchForMovies"
+                v-on:keyup="searchForMovies"
                 style="position: absolute; width: 30%; top: 5px"
                 v-on:blur="timedCloseMenu"
         >
         </v-text-field>
-        <v-list v-if="searchResponse.length > 0" style="position: absolute; top: 6vh; width: 30%">
+        <Loading v-if="isLoading && searching"/>
+        <v-list v-else-if="searchResponse.length > 0 && !isLoading" style="position: absolute; top: 6vh; width: 30%">
             <SearchResult v-for="search in searches"
                           :key="search.imdbID"
                           :poster="search.Poster"
                           :title="search.Title"
                           :year="search.Year"
                           :id="search.imdbID"
-                           v-on:closemenu="closeMenu"/>
+                          v-on:closemenu="closeMenu"/>
         
         </v-list>
-    
+        <v-container v-else-if="!isLoading && this.search.length > 0">No movies found</v-container>
     </v-container>
 </template>
 
 <script>
   import SearchResult from "./SearchResult";
+  import Loading from '@/components/Loading';
+
 
   export default {
     name: "SearchInput",
-    components: {SearchResult},
+    components: {Loading, SearchResult},
     data() {
       return {
         search: "",
-        searchResponse: []
+        searchResponse: [],
+        loading: true,
+        searching: false
       }
     },
     methods: {
       async searchForMovies() {
-        let response = await fetch('api/movies/search?s=' + this.search);
-        response = response.status === 200 ? await response.json() : null;
-        this.searchResponse = response.Search;
-        return response;
+        this.searching = true;
+        this.loading = true;
+        setTimeout(async () => {
+          let response = await fetch('api/movies/search?s=' + this.search);
+          response = response.status === 200 ? await response.json() : null;
+          this.searchResponse = response ? response.Search : [];
+          this.loading = false;
+          this.searching = false;
+          return response;
+        }, 1000);
       },
       closeMenu() {
         this.search = "";
@@ -55,6 +66,9 @@
     computed: {
       searches() {
         return this.searchResponse;
+      },
+      isLoading() {
+        return this.loading;
       }
     }
   }
