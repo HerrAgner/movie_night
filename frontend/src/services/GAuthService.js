271f3be0660404253e25ec66821100cd900e461d
Martin Waldemarsson <martin.waldemarsson@omegapoint.se>
2019-12-05 15:54:32 +0100

    Removed console.log

--- frontend/src/services/GAuthService.js
+++ frontend/src/services/GAuthService.js
@@ -1,7 +1,7 @@
-export default () => ({
-  async getMovieDetails(id) {
-    let response = await fetch('api/movies?i=' + id);
-    response = response.status === 200 ? response.json() : null;
-    return response;
-  }
-});
+// export default () => ({
+//   async getMovieDetails(id) {
+//     let response = await fetch('api/movies?i=' + id);
+//     response = response.status === 200 ? response.json() : null;
+//     return response;
+//   }
+// });
